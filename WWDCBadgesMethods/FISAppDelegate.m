450eeb2655d0754e18de09131eb3bfff9e73159c
imryan <notryancohen@gmail.com>
2016-05-15 18:47:48 -0400

    done

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -11,14 +11,30 @@
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    
     return YES;
 }
 
-/*
- 
- * Define your methods between application:didFinishLaunchingWithOptions and the @end marker
- 
- */
+- (NSString *)badgeForSpeaker:(NSString *)speaker {
+    return [NSString stringWithFormat:@"Hello, my name is %@.", speaker];
+}
+
+- (NSArray *)badgesForSpeakers:(NSArray *)speakers {
+    NSMutableArray *formattedSpeakers = [NSMutableArray array];
+    for (NSString *speaker in speakers) {
+        [formattedSpeakers addObject:[self badgeForSpeaker:speaker]];
+    }
+    
+    return formattedSpeakers;
+}
+
+- (NSArray *)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers {
+    NSMutableArray *formattedRooms = [NSMutableArray array];
+    for (NSUInteger i = 0; i < [speakers count]; i++) {
+        NSString *speaker = speakers[i];
+        [formattedRooms addObject:[NSString stringWithFormat:@"Welcome, %@! You'll be in dressing room %lu.", speaker, i + 1]];
+    }
+    
+    return formattedRooms;
+}
 
 @end
